00845042f5613a5f5eea528f9416a13aae3f9fe2
Douglas Phelipe <douglasphelipedpss@gmail.com>
2025-12-22 22:26:31 -0300

    feat: delete backend.

--- backend/src/views/availabilities.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-from datetime import date, datetime
-
-class CreateAvailability(BaseModel):
-    date: date
-    start_time: datetime
-    end_time: datetime
-    slot_duration_minutes: int

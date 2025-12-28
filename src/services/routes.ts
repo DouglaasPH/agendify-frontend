4d7469c2395b7dbe12e139c6a9dd827303f56406
Douglas Phelipe <douglasphelipedpss@gmail.com>
2025-12-28 13:27:45 -0300

    style: remove unnecessary comments

--- src/services/routes.ts
+++ src/services/routes.ts
@@ -1,89 +1,44 @@
-/* export const ROUTES = {
-  auth: {
-    login: "/auth/",
-    register: "/auth/register",
-    registerGenerateVerificationToken:
-      "/auth/register/generate-verification-token",
-    refresh: "/auth/refresh",
-    logout: "/auth/logout",
-    delete: "/auth/delete",
-    checkEmail: (email: string) => `/auth/check-email/${email}`,
-    getUserData: "/auth/",
-    modifyUserData: "/auth/modify-user-data",
-    sendEmailToChangeEmail: "/auth/send-email-to-change-email",
-    modifyUserEmail: "/auth/modify-user-email",
-    modifyUserPassword: "/auth/modify-user-password",
-    forgotYourPassword: "auth/forgot-your-password",
-    forgotYourPasswordResetPassword:
-      "/auth/forgot-your-password/reset-password",
-    getUserDataForCustomer: (chat_code: string) => `/auth/${chat_code}`,
-  },
-  appointment: {
-    create: "/appointment",
-    list: "/appointment/",
-    get: (id: number) => `/appointment/${id}`,
-    cancel: (id: number) => `/appointment/${id}`,
-  },
-  availability: {
-    create: "/availability",
-    list: "/availability",
-    get: (id: number) => `/availability/${id}`,
-    delete: (id: number) => `/availability/${id}`,
-  },
-  customer: {
-    registerOrLoginWithoutCustomerId: "/customer/",
-    loginWithCustomerId: (id: number) => `/customer/${id}`,
-    allAppointments: "/customer/appointment",
-    createAppointment: "/customer/appointment/create",
-    cancelAppointment: (appointmend_id: number) =>
-      `/customer/appointment/${appointmend_id}`,
-    allAvailabilities: (professional_id: number) =>
-      `/customer/availability/${professional_id}`,
-  },
-};
-*/
-
 export const ROUTES = {
-  refresh_token: "/refresh-token", // OK
+  refresh_token: "/refresh-token",
   professional: {
-    check_email: (email: string) => `/professional/check-email/${email}`, // OK
-    get_data_by_id_via_access_token: "/professional", // OK
-    get_data_by_chat_code: (chat_code: string) => `/professional/${chat_code}`, // OK
-    modify_data: "/professional/modify-data", // OK
-    send_email_to_change_email: "/professional/send-email-to-change-email", // OK
+    check_email: (email: string) => `/professional/check-email/${email}`,
+    get_data_by_id_via_access_token: "/professional",
+    get_data_by_chat_code: (chat_code: string) => `/professional/${chat_code}`,
+    modify_data: "/professional/modify-data",
+    send_email_to_change_email: "/professional/send-email-to-change-email",
     send_email_to_change_password:
-      "/professional/send-email-to-change-password", // OK
-    confirm_email_modification: "/professional/confirm-email-modification", // OK
+      "/professional/send-email-to-change-password",
+    confirm_email_modification: "/professional/confirm-email-modification",
     confirm_password_modification:
       "/professional/confirm-password-modification", //OK
-    generate_verification_token: "/professional/generate-verification-token", // OK
-    register: "/professional/register", // OK
-    login: "/professional/login", // OK
-    logout: "/professional/logout", // OK
-    delete: "/professional/delete", // OK
+    generate_verification_token: "/professional/generate-verification-token",
+    register: "/professional/register",
+    login: "/professional/login",
+    logout: "/professional/logout",
+    delete: "/professional/delete",
     modify_password_with_login: "/professional/modify-password-with-login",
   },
   customer: {
-    login_with_id: (customer_id: number) => `/customer/login/${customer_id}`, // OK
-    register_or_login: "/customer/", // OK
+    login_with_id: (customer_id: number) => `/customer/login/${customer_id}`,
+    register_or_login: "/customer/",
   },
   availability: {
-    create_by_professional: "/availability/professional/create", // OK
+    create_by_professional: "/availability/professional/create",
     get_by_id_for_professional: (availability_id: number) =>
-      `/availability/professional/get/${availability_id}`, // OK
-    list_for_professional: "/availability/professional/list", // OK
-    list_for_customer: "/availability/customer/list", // OK
+      `/availability/professional/get/${availability_id}`,
+    list_for_professional: "/availability/professional/list",
+    list_for_customer: "/availability/customer/list",
     delete_by_id_via_professional: (availability_id: number) =>
-      `/availability/professional/delete/${availability_id}`, // OK
+      `/availability/professional/delete/${availability_id}`,
   },
   appointment: {
-    create_by_customer: "/appointment/customer/create", // OK
+    create_by_customer: "/appointment/customer/create",
     cancel_by_id_via_customer: (appointment_id: number) =>
-      `/appointment/customer/cancel/${appointment_id}`, // OK
+      `/appointment/customer/cancel/${appointment_id}`,
     cancel_by_id_via_professional: (appointment_id: number) =>
-      `/appointment/professional/cancel/${appointment_id}`, // OK
-    list_for_customer: "/appointment/customer/list", // OK
-    list_for_professional: "/appointment/professional/list", // OK
+      `/appointment/professional/cancel/${appointment_id}`,
+    list_for_customer: "/appointment/customer/list",
+    list_for_professional: "/appointment/professional/list",
     get_by_id_for_professional: (appointment_id: number) =>
       `/appointment/professional/get/${appointment_id}`,
   },

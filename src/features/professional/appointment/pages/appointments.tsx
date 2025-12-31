8be5a13f848b06e26f78325231c0ac3ef0ee25df
Douglas Phelipe <douglasphelipedpss@gmail.com>
2025-12-30 21:22:25 -0300

    fix(build): fix import case for TitleAndStatus

--- src/features/professional/appointment/pages/appointments.tsx
+++ src/features/professional/appointment/pages/appointments.tsx
@@ -19,7 +19,7 @@ import {
 } from "@/shared/utils/utils";
 
 // components
-import TitleAndStatus from "@/features/professional/appointment/components/TitleAndStatus.tsx";
+import TitleAndStatus from "@/features/professional/appointment/components/TitleAndStatus";
 import Filters from "../components/Filters";
 import AppointmentSchedule from "../components/AppointmentsSchedule";
 import PaginationComponent from "@/shared/components/Pagination";
